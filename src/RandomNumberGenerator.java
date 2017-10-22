8bd7535f1f9659e61cc9dde1a5337794a67438bb
yxguo7 <32585152+yxguo7@users.noreply.github.com>
2017-10-22 13:47:45 -0700

    generate 1000 uniformly distributed int value within range (0 - 1000000)

--- src/RandomNumberGenerator.java
+++ src/RandomNumberGenerator.java
@@ -1,10 +1,15 @@
+/**
+ * Created by Yuxiao Guo
+ * generate 1000 uniformly distributed int value within range (0 - 1000000)
+ */
+
 import java.io.*;
 import java.util.Random;
 
 public class RandomNumberGenerator {
 
         public static void main(String... aArgs) throws IOException {
-            //generate 1000 uniformly distributed int value within range (0 - 1000000)
+
             Writer wr = new FileWriter("RandomNumber1.txt");
             Random randomGenerator = new Random();
             for (int idx = 1; idx <= 1000; ++idx) {
--- src/Sorter.java
+++ src/Sorter.java
@@ -26,7 +26,95 @@ public class Sorter {
 
     public void bubble(int[] array){}
 
-    public void merge(int[] array){}
 
-    public void quike(int[] array){}
+
+    /**
+     * created by Yuxiao Guo
+     * @param array
+     */
+    public void merge(int[] array){
+        // use res array to temperately store the result.
+        int[] res = new int[array.length];
+        mergeSort(array, 0, array.length - 1, res);
+    }
+
+    private void mergeSort(int[] array, int start, int end, int[] res) {
+        if (start >= end) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+
+        mergeSort(array, start, mid, res);
+        mergeSort(array, mid+1, end, res);
+        merge(array, start, mid, end, res);
+    }
+
+    private void merge(int[] array, int start, int mid, int end, int[] res) {
+        int left = start;
+        int right = mid+1;
+        int index = start;
+
+        // merge two sorted subarrays to result array
+        while (left <= mid && right <= end) {
+            if (array[left] < array[right]) {
+                res[index++] = array[left++];
+            } else {
+                res[index++] = array[right++];
+            }
+        }
+        while (left <= mid) {
+            res[index++] = array[left++];
+        }
+        while (right <= end) {
+            res[index++] = array[right++];
+        }
+
+        // copy result back to original array
+        for (index = start; index <= end; index++) {
+            array[index] = res[index];
+        }
+    }
+
+    /**
+     * created by Yuxiao Guo
+     * @param array
+     */
+    public void quick(int[] array){
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private void quickSort(int[] array, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int left = start, right = end;
+        // choose middle point as pivot
+        int pivot = array[(start + end) / 2];
+
+        //put all element that <= pivot to the pivot's left side
+        //put all element that > pivot to the pivot's right side
+        while (left <= right) {
+            // key point 3: array[left] < pivot not array[left] <= pivot
+            while (left <= right && array[left] < pivot) {
+                left++;
+            }
+            // key point 3: array[right] > pivot not array[right] >= pivot
+            while (left <= right && array[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                int temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+        //recursively sort left and right subarray.
+        quickSort(array, start, right);
+        quickSort(array, left, end);
+    }
 }
